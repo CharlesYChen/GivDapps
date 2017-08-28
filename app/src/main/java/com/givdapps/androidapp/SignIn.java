a3ff9f90b8ef9701d72b67f1544a86f54d55fc3e
Charles Chen <cc3dr@virginia.edu>
2017-08-27 23:47:31 -0400

    Final upload for the summer; needs database and stripe integration.

--- app/src/main/java/com/givdapps/androidapp/SignIn.java
+++ app/src/main/java/com/givdapps/androidapp/SignIn.java
@@ -5,11 +5,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.FileInputStream;
 
 public class SignIn extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sign_in);
@@ -21,28 +26,24 @@ public class SignIn extends AppCompatActivity {
 
     public void signinButton(View view){
 
-        //uncomment below to prevent blank signin attempt
+        EditText email_address = (EditText)findViewById(R.id.email_address);
+        EditText enter_password = (EditText)findViewById(R.id.enter_password);
 
-//        EditText email_address = (EditText)findViewById(R.id.email_address);
-//        EditText enter_password = (EditText)findViewById(R.id.enter_password);
-//
-//        String email = email_address.getText().toString();
-//        String password = enter_password.getText().toString();
-//        if (email.trim().length() == 0 && password.trim().length()==0) {
-//            Toast.makeText(this, "Please enter your email and password.", Toast.LENGTH_SHORT).show();
-//        }
-//        else if (email.trim().length() == 0) {
-//            Toast.makeText(this, "Please enter your email.", Toast.LENGTH_SHORT).show();
-//        }
-//        else if(password.trim().length()==0){
-//            Toast.makeText(this, "Please enter your password.", Toast.LENGTH_SHORT).show();
-//        }
-//        else {
-//            Intent intent = new Intent(this, IntroScreen.class);
-//            startActivity(intent);
-//        }
-        Intent intent = new Intent(this, CausesNavigationDrawer.class);
-        startActivity(intent);
+        String email = email_address.getText().toString();
+        String password = enter_password.getText().toString();
+        if (email.trim().length() == 0 && password.trim().length()==0) {
+            Toast.makeText(this, "Please enter your email and password.", Toast.LENGTH_SHORT).show();
+        }
+        else if (email.trim().length() == 0) {
+            Toast.makeText(this, "Please enter your email.", Toast.LENGTH_SHORT).show();
+        }
+        else if(password.trim().length()==0){
+            Toast.makeText(this, "Please enter your password.", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            Intent intent = new Intent(this, CausesNavigationDrawer.class);
+            startActivity(intent);
+        }
     }
     public void getNewPasswordButton (View view){
         Intent intent = new Intent(this, NewPassword.class);
